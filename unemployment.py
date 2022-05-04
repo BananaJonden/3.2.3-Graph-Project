166300275a72e0a1ec93341aba1a9461521e268c
BananaJonden <91635129+BananaJonden@users.noreply.github.com>
2022-05-04 13:57:56 -0700

    Update unemployment.py

--- unemployment.py
+++ unemployment.py
@@ -1,4 +1,3 @@
-
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -7,49 +6,68 @@ import pandas as pd
 my_cities = ['Agoura Hills City', 'Yorba Linda City', 'San Jose City', 'San Diego City', "Palm spring City", "Bakersfield City"]
 
 
-df = pd.read_csv("Local_Area_Unemployment_Statistics__LAUS___Annual_Average.csv", header=0)    # header=0 means there is a header in row 0
+unemployment = pd.read_csv("Local_Area_Unemployment_Statistics__LAUS___Annual_Average.csv", header=0)    # header=0 means there is a header in row 0
 
-unique_cities = df['Area Name'].unique()
+unique_cities = unemployment['Area Name'].unique()
 
 for c in unique_cities:
   if c in my_cities:
     
-    years = df[df['Area Name'] == c]['Year']
+    years = unemployment[unemployment['Area Name'] == c]['Year']
 
-    sum_unemployment = df[df['Area Name'] == c]['Unemployment Rate']
+    sum_unemployment = unemployment[unemployment['Area Name'] == c]['Unemployment Rate']
 
     plt.plot(years, sum_unemployment, label=c)
 
 
-# TODO #5: Calculate min, max, and avg anomaly and the corresponding min/max years
-
-min_anomaly = temp_data['Anomaly'][0]
-max_anomaly = temp_data['Anomaly'][0]
-min_year = temp_data['Year'][0]
-max_year = temp_data['Year'][0]
-sum_anomaly = 0
-avg_anomaly = 0
+'''
+min_unemployment = unemployment['Unemployment Rate'][0]
+max_unemployment = unemployment['Unemployment Rate'][0]
+min_year = unemployment['Year'][0]
+max_year = unemployment['Year'][0]
+sum_unemployment = 0
+avg_unemployment = 0
 
 # find the min, max and calculate the sum
-for i in range(len(temp_data['Anomaly'])):
-  if (temp_data['Anomaly'][i] < min_anomaly):
-    min_anomaly = temp_data['Anomaly'][i]
-    min_year = temp_data['Year'][i]
+for i in range(len(unemployment['Unemployment Rate'])):
+  if (unemployment['Unemployment Rate'][i] < min_unemployment):
+    min_unemployment = unemployment['Unemployment Rate'][i]
+    min_year = unemployment['Year'][i]
 
       # new code inside the same for loop
-  if (temp_data['Anomaly'][i] > max_anomaly):
-    max_anomaly = temp_data['Anomaly'][i]
-    max_year = temp_data['Year'][i]
+  if (unemployment['Unemployment Rate'][i] > max_unemployment):
+    max_unemployment = unemployment['Unemployment Rate'][i]
+    max_year = unemployment['Year'][i]
 
-    sum_anomaly += temp_data['Anomaly'][i]
+    sum_unemployment += unemployment['Unemployment Rate'][i]
 
 # calculate average
-avg_anomaly = sum_anomaly/len(temp_data['Anomaly'])
+avg_unemployment = sum_unemployment/len(unemployment['Unemployment Rate'])
 
+'''
 
+palm_spring_average = (11.8+11.1+9.8+8.3+6.8+5.5+5.6+4.7+4.1+3.8+10.7)
+agoura_hills_average = (10.5+10.2+9.2+8.1+6.8+5.5+4.8+4.1+4.3+4+10.3)
+yorba_linda_average = (8.8+8.2+7.1+5.9+4.9+3.9+3.7+3.3+2.8+2.6+7.2)
+san_jose_average = (11.8+10.5+8.9+7.3+5.8+4.7+3.9+3.4+2.8+2.7+8.1)
+san_diego_average = (10.5+10+8.8+7.6+6.2+5+4.6+3.9+3.+ 3.1+9)
+bakersfield_average = (14.4+13.5+12+10.6+9.4+9.2+7.8+6.5+5.6+5.4+11.2)
+
+minimum = 2.6
+maximum = 14.4
+total_average = palm_spring_average + agoura_hills_average + yorba_linda_average + san_jose_average + san_diego_average + bakersfield_average
+total_average = total_average/55
+print("the total average of unemployment:",total_average)
+print("the minimum:", minimum)
+print("the maximum:", maximum)
 
 plt.ylabel('Percentage of City Unemployment Rate')
 plt.xlabel('Year')
 plt.title('Percent of Population With Unemployment')
 plt.legend()
 plt.show()
+'''
+print("The maximum Unemployment:", max_unemployment, "which occured in", max_year)
+print("The minimum Unemployment:", min_unemployment, "which occured in", min_year)
+print("The average Unemployment:", avg_unemployment)
+'''
